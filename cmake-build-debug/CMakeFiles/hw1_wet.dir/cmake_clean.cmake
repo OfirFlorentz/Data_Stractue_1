13c3939919614450a9d4f42fc2cc923133fc979a
ofirFlorentz <Ofir9791>
2019-11-25 19:32:06 +0200

    test

---
+++ cmake-build-debug/CMakeFiles/hw1_wet.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw1_wet.dir/libary1.cpp.obj"
+  "CMakeFiles/hw1_wet.dir/main.cpp.obj"
+  "hw1_wet.pdb"
+  "hw1_wet.exe"
+  "hw1_wet.exe.manifest"
+  "libhw1_wet.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw1_wet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ libary1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ofir on 24/11/2019.
+//
+
+#include "libary1.h"
