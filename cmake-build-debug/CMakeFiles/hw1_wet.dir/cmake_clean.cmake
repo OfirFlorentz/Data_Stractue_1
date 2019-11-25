8751c00fd4ea00ef6d0fa3b7e2459fe7e7466b60
Nathan Voldman <nathan.vmail@gmail.com>
2019-11-25 22:49:09 +0200

    hello

--- cmake-build-debug/CMakeFiles/hw1_wet.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/hw1_wet.dir/cmake_clean.cmake
@@ -1,10 +1,8 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/hw1_wet.dir/libary1.cpp.obj"
-  "CMakeFiles/hw1_wet.dir/main.cpp.obj"
+  "CMakeFiles/hw1_wet.dir/libary1.cpp.o"
+  "CMakeFiles/hw1_wet.dir/main.cpp.o"
   "hw1_wet.pdb"
-  "hw1_wet.exe"
-  "hw1_wet.exe.manifest"
-  "libhw1_wet.dll.a"
+  "hw1_wet"
 )
 
 # Per-language clean rules from dependency scanning.
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/ofir/Desktop/courses/20-winter/data_stracture/hw1_wet
+# Install script for directory: /mnt/c/Users/Nathan/CLionProjects/Data_Stractue_1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/hw1_wet")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -27,6 +27,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -40,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/ofir/Desktop/courses/20-winter/data_stracture/hw1_wet/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/mnt/c/Users/Nathan/CLionProjects/Data_Stractue_1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
