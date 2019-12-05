63f1aef521290c78f41b8f5b5542a9df11acb2cb
Nathan Voldman <nathan.vmail@gmail.com>
2019-12-05 16:11:50 +0200

    AVL Tree fix remove again

--- library1.cpp
+++ library1.cpp
@@ -11,22 +11,37 @@ void * Init(){
 }
 
 StatusType AddDataCenter(void *DS, int dataCenterID, int numOfServers) {
+    if (DS == NULL){
+        return INVALID_INPUT;
+    }
     return ((DCM *)DS)-> addDataCenter(dataCenterID, numOfServers);
 }
 
 StatusType RemoveDataCenter(void *DS, int dataCenterID) {
+    if (DS == NULL){
+        return INVALID_INPUT;
+    }
     return ((DCM *)DS)-> removeDataCenter(dataCenterID);
 }
 
 StatusType RequestServer(void *DS, int dataCenterID, int serverID, int os, int *assignedID){
+    if (DS == NULL){
+        return INVALID_INPUT;
+    }
     return ((DCM *) DS)-> requestServer(dataCenterID,serverID,os,assignedID);
 }
 
 StatusType FreeServer(void *DS, int dataCenterID, int serverID){
+    if (DS == NULL){
+        return INVALID_INPUT;
+    }
     return ((DCM *) DS)-> freeServer(dataCenterID,serverID);
 }
 
 StatusType GetDataCentersByOS(void *DS, int os, int **dataCenters, int* numOfDataCenters){
+    if (DS == NULL){
+        return INVALID_INPUT;
+    }
     return ((DCM *) DS)->GetDataCentersByOS(os,dataCenters,numOfDataCenters);
 }
 
