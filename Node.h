bfebcf50c685c062982f8d4a3e519bf5c5ca3165
ofirFlorentz <Ofir9791>
2019-12-08 22:27:19 +0200

    all check and works. with comments

--- Node.h
+++ Node.h
@@ -1,20 +1,18 @@
-//
-// Created by ofir on 26/11/2019.
-//
+/**This class is hold id of server and the amount of servers he have from speicfic type**/
 
 #ifndef HW1_WET_NODE_H
 #define HW1_WET_NODE_H
 
 #include <iostream>
 
-class ValueNode;
-class DCNode;
-
 class DCNode{
     int id;
     int value;
 public:
+    // constructor
     DCNode (int id, int value);
+    // comparing operators overloading
+    // comparing is first by value and then by id
     friend bool operator >(const DCNode&, const DCNode&);
     friend bool operator >(DCNode&, DCNode&);
     friend bool operator ==(const DCNode& first, const DCNode& second);
